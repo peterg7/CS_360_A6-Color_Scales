0db6455f5294a59abd60093bc4519ad4ff26e30c
Peter Gish <pgish97@gmail.com>
2022-03-15 22:01:43 -0700

    final update

--- diverging_bar/div_bar_chart.js
+++ diverging_bar/div_bar_chart.js
@@ -1,22 +1,28 @@
 // NOTE: Referenced https://observablehq.com/@d3/diverging-stacked-bar-chart
 //                  https://bl.ocks.org/tlfrd/187e45e0629711c4560cf6bcd0767b27
 
+/**********************
+ ***    Constants   ***
+ **********************/
 
 // set the dimensions and margins of the graph
 const SCREEN_DIMENSIONS = { 
     width: 600,
-    height: 600,
+    height: 500,
     leftMargin: 30,
-    rightMargin: 115,
-    topMargin: 10,
-    bottomMargin: 100,
+    rightMargin: 135,
+    topMargin: 20,
+    bottomMargin: 150,
     cellPadding: 20
 };
 SCREEN_DIMENSIONS.innerWidth = SCREEN_DIMENSIONS.width - SCREEN_DIMENSIONS.leftMargin - SCREEN_DIMENSIONS.rightMargin;
 SCREEN_DIMENSIONS.innerHeight = SCREEN_DIMENSIONS.height - SCREEN_DIMENSIONS.topMargin - SCREEN_DIMENSIONS.bottomMargin;
 
+const TITLE_TEXT = "2010 Sampling of Country's Energy Usage";
+const DATA_LOC = '../data/transformed/generated_consumption_production_by_source.csv';
+
+// Control parameters
 const NUM_COUNTRIES = 10;
-const DATA_LOC = '../data/generated_consumption_production_by_source.csv';
 const MIN_YEAR = 2010,
         MAX_YEAR = 2020;
 
@@ -29,101 +35,112 @@ var svg = d3.select('body').append('svg')
     .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
 
 const columnTranslations = new Map([
-    ['Entity', 'country'],
-    ['Year', 'year'],
-    ['footprint_ratio', 'footprint'],
-    ['source', 'source']
+    ['entity', 'country'],
+    ['year', 'year'],
+    ['cp_ratio', 'cp_ratio'],
+    ['cp_ratio_relative_to_source', 'cp_ratio_relative_to_source'],
+    ['source', 'source'],
+    ['renewable', 'renewable'],
+    ['consumption_relative_to_source', 'consumption_relative_to_source'],
+    ['production_relative_to_source', 'production_relative_to_source']
 ]);
 
 
-/**
- * LAYOUT/OVERVIEW
- * 
- * x -> fooprint_ratio
- * y -> Entity (country)
- * z (color) -> source
- */
-
+/**********************
+ * Feature parameters *
+ **********************/
 
 const xParams = {
-    value: row => row.footprint,
-    label: 'Footprint (cons / prod)',
-    // scale: d3.scaleBand(),
-    tickPadding: 5
+    value: row => row.cp_ratio_relative_to_source,
+    label: 'Ratio of Energy Consumption to Production',
+    scale: d3.scaleLinear(),
+    tickPadding: 4
 };
-// xParams.axis = d3.axisBottom()
-//     .scale(xParams.scale)
-//     .tickPadding(xParams.tickPadding)
-//     .tickFormat(d3.format('+%'))
-//     .tickSize(-SCREEN_DIMENSIONS.innerHeight);
+xParams.axis = d3.axisBottom()
+    .scale(xParams.scale)
+    .tickSizeOuter(0);
 
 const yParams = {
     value: row => row.country,
     label: 'Country',
-    // scale: d3.scaleLinear(),
+    scale: d3.scaleBand(),
     tickPadding: 5
 };
-// yParams.axis = d3.axisLeft()
-//     .scale(yParams.scale)
-//     .tickPadding(yParams.tickPadding)
-//     .tickFormat(d3.format('.2f'))
-//     .tickSize(-SCREEN_DIMENSIONS.innerWidth)
-
 
 const colorParams = {
-    value: row => row.source
-} 
+    value: row => row.production_relative_to_source,
+    label1: "Relative Production of",
+    label2: "Renewable Energy",
+    pre_scale: d3.scaleLinear().range([0, 1])
+}
+colorParams.colorMap = x => d3.scaleDiverging(d3.interpolateRdYlGn)(colorParams.pre_scale(x))
 
-const colorArray = d3.schemeTableau10;
-const stackOffset = d3.stackOffsetDiverging // stack offset method
-const stackOrder = (series) => { // stack order method; try also d3.stackOffsetNone
-    return [ // by default, stack negative series in reverse order
-        ...series.map((S, i) => S.some(([, y]) => y < 0) ? i : null).reverse(),
-        ...series.map((S, i) => S.some(([, y]) => y < 0) ? null : i)
-    ].filter(i => i !== null);
+/******************
+ * Graphic groups *
+ ******************/
+
+const groupTranslations = {
+    'canvas': `translate(${SCREEN_DIMENSIONS.leftMargin},${SCREEN_DIMENSIONS.topMargin})`,
+    'xAxis': `translate(0, ${SCREEN_DIMENSIONS.innerHeight})`,
+    'title': `translate(${(SCREEN_DIMENSIONS.innerWidth / 2)},${0 - (SCREEN_DIMENSIONS.topMargin * 1.35)})`,
+    'legend': `translate(${SCREEN_DIMENSIONS.innerWidth + SCREEN_DIMENSIONS.rightMargin / 5},${SCREEN_DIMENSIONS.topMargin * 2})`
 }
 
+
 const canvas = svg.append('g')
-    .attr('transform', `translate(${SCREEN_DIMENSIONS.leftMargin},${SCREEN_DIMENSIONS.topMargin})`);
+                    .attr('transform', groupTranslations.canvas);
 
-// const xAxisGroup = canvas.append('g')
-//     .attr('transform', `translate(0, ${SCREEN_DIMENSIONS.innerHeight})`);
+const xAxisGroup = canvas.append('g')
+                            .attr('class', 'x-axis')
+                            .attr('transform', groupTranslations.xAxis)
 
-// const yAxisGroup = canvas.append('g');
+const yAxisGroup = canvas.append('g')
+                            .attr('class', 'y-axis');
 
-// const titleGroup = canvas.append("text")
-//     .attr('transform', `translate(${(SCREEN_DIMENSIONS.innerWidth / 2)},${0 - (SCREEN_DIMENSIONS.topMargin * 1.35)})`)
-//     .attr("class", "title")
-//     .text("US Energy Production (Solar vs. Oil) 2010-2020");
 
-// const legendGroup = canvas.append('g')
-//     .attr("transform", `translate(${SCREEN_DIMENSIONS.innerWidth + SCREEN_DIMENSIONS.rightMargin / 5},${SCREEN_DIMENSIONS.topMargin * 2})`)
-//     .attr("class", "legendSequential");
+const titleGroup = canvas.append("text")
+                            .attr('transform', groupTranslations.title)
+                            .attr("class", "title")
+                            .text(TITLE_TEXT);
 
-// xAxisGroup.append('text')
-//     .attr('class', 'x-axis-label')
-//     .attr('x', SCREEN_DIMENSIONS.innerWidth / 2)
-//     .attr('y', 50)
-//     .text(xParams.label);
+// Append a linearGradient element 
+var linearGradient = canvas.append("linearGradient")
+                            .attr("id", "linear-gradient");
 
-// yAxisGroup.append('text')
-//     .attr('class', 'y-axis-label')
-//     .attr('x', -SCREEN_DIMENSIONS.topMargin * 5.5)
-//     .attr('y', -SCREEN_DIMENSIONS.leftMargin * 1.75)
-//     .text(yParams.label);
+linearGradient
+        .attr("x1", "0%")
+        .attr("y1", "0%")
+        .attr("x2", "0%")
+        .attr("y2", "100%");
 
+
+var bars = canvas.append("g")
+    .attr("class", "bars")
+
+var barLabels = canvas.append("g")
+    .attr("class", "labels");
+
+xAxisGroup.append('text')
+        .attr('class', 'x-axis-label')
+        .attr('x', SCREEN_DIMENSIONS.innerWidth / 2)
+        .attr('y', 50)
+        .text(xParams.label)
+
+/*********************
+ * Data manipulation *
+ *********************/
 
 const preprocess = (row, i) => {
     let filtered = d => { 
-        let yr = parseInt(d['Year']) 
+        let yr = parseInt(d['year']) 
         return Object.values(d).includes(null) ||
-                    yr < MIN_YEAR || yr > MAX_YEAR ||
-                    Object.values(d).includes(-1) // represents infinity (invalid)
+                    d['source'] != 'agg_renew' ||
+                    yr != 2010
     };
     return !filtered(row) ? Object.fromEntries(
         Array.from(columnTranslations.entries())
             .map(([key, val]) => {
-                if (['Entity', 'source'].includes(key))
+                if (['entity', 'source', 'renewable'].includes(key))
                     return [val, row[key]]
                 return [val, parseFloat(row[key])]
             })
@@ -147,136 +164,126 @@ d3.csv(DATA_LOC, preprocess).then((data, i) => {
     let sampleData = data.filter(a => randomCountries.includes(a.country))
     let groupedSampleData = groupBy(sampleData, 'country')
 
-
-    // Compute values.
-    const X = d3.map(sampleData, xParams.value);
-    const Y = d3.map(sampleData, yParams.value);
-    const Z = d3.map(sampleData, colorParams.value);
-
     console.log(sampleData)
+    console.log(groupedSampleData)
 
+    
+    xParams.scale.domain(d3.extent(sampleData, xParams.value))
+                    .range([0, SCREEN_DIMENSIONS.innerWidth]);
 
-    let yDomain = new Set(d3.groupSort(sampleData, D => d3.sum(D, d => -Math.min(0, xParams.value(d))), d => yParams.value(d)))
-    let zDomain = new Set(sampleData.map(a => a.source))
+    yParams.scale.domain(sampleData.map(yParams.value))
+                    .range([SCREEN_DIMENSIONS.innerHeight, 0])
+                    .padding(0.1);
 
-
-
-    // Omit any data not present in the y- and z-domains.
-    const I = d3.range(X.length).filter(i => yDomain.has(Y[i]) && zDomain.has(Z[i]));
-
+    let minColor = d3.min(sampleData, colorParams.value)
+    let maxColor = d3.max(sampleData, colorParams.value)
+    colorParams.pre_scale.domain([minColor, maxColor])
 
     
 
-    
-    // Compute a nested array of series where each series is [[x1, x2], [x1, x2],
-    // [x1, x2], …] representing the x-extent of each stacked rect. In addition,
-    // each tuple has an i (index) property so that we can refer back to the
-    // original data point (data[i]). This code assumes that there is only one
-    // data point for a given unique y- and z-value.
-    const series = d3.stack()
-        .keys(zDomain)
-        .value(([, I], z) => X[I.get(z)])
-        .order(stackOrder)
-        .offset(stackOffset)
-        (d3.rollup(I, ([i]) => i, i => Y[i], i => Z[i]))
-        .map(s => s.map(d => Object.assign(d, {i: d.data[1].get(s.key)})));
-    
-    let xDomain = d3.extent(series.flat(2));
+    // Set legend colors
+    linearGradient.selectAll("stop")
+        .data([
+            {offset: "0%", color: colorParams.colorMap(minColor)},
+            {offset: "25%", color: colorParams.colorMap(((maxColor - minColor) / 4) + minColor)},
+            {offset: "50%", color: colorParams.colorMap(((maxColor - minColor) / 2) + minColor)},
+            {offset: "75%", color: colorParams.colorMap(((maxColor - minColor) / 4) - maxColor)},
+            {offset: "100%", color: colorParams.colorMap(maxColor)}
+            ])
+        .enter().append("stop")
+        .attr("offset", d => d.offset)
+        .attr("stop-color", d => d.color);
 
-    // Compute the default y-domain. Note: diverging stacks can be negative.
-    const xRange = [SCREEN_DIMENSIONS.leftMargin, SCREEN_DIMENSIONS.innerWidth - SCREEN_DIMENSIONS.rightMargin];
-    const yRange = [SCREEN_DIMENSIONS.height - SCREEN_DIMENSIONS.bottomMargin, SCREEN_DIMENSIONS.topMargin];
 
-    // Construct scales, axes, and formats.
-    const xScale = d3.scaleLinear(xDomain, xRange);
-    const yScale = d3.scaleBand(yDomain, yRange).paddingInner(0.1);
-    const colorScale = d3.scaleOrdinal(zDomain, colorArray);
-    // const xAxis = d3.axisTop(xScale).ticks(SCREEN_DIMENSIONS.innerWidth / 80, xFormat);
-    // const yAxis = d3.axisLeft(yScale).tickSize(0);
+    bars.selectAll("rect")
+        .data(sampleData)
+        .enter()
+        .append("rect")
+            .attr("class", "bar")
+            .attr("x", d => xParams.scale(Math.min(0, xParams.value(d))))
+            .attr("y", d => yParams.scale(yParams.value(d)))
+            .attr("height", yParams.scale.bandwidth())
+            .attr("width", d => Math.abs(xParams.scale(xParams.value(d)) - xParams.scale(0)))
+            .style("fill", d => colorParams.colorMap(colorParams.value(d)));
+        
 
-    // canvas.append("g")
-    //     .attr("transform", `translate(0,${marginTop})`)
-    //     .call(xAxis)
-    //     .call(g => g.select(".domain").remove())
-    //     .call(g => g.selectAll(".tick line").clone()
-    //         .attr("y2", height - marginTop - marginBottom)
-    //         .attr("stroke-opacity", 0.1))
-    //     .call(g => g.append("text")
-    //         .attr("x", xScale(0))
-    //         .attr("y", -22)
-    //         .attr("fill", "currentColor")
-    //         .attr("text-anchor", "middle")
-    //         .text(xLabel));
+    barLabels.selectAll("text")
+        .data(sampleData)
+        .enter()
+        .append("text")
+            .attr("class", "bar-label")
+            .attr("x", xParams.scale(0))
+            .attr("y", d => yParams.scale(yParams.value(d)) - yParams.scale.bandwidth()/2)
+            .attr("dx", d => xParams.value(d) < 0 ? xParams.tickPadding : -xParams.tickPadding)
+            .attr("dy", yParams.scale.bandwidth())
+            .attr("text-anchor", d => xParams.value(d) < 0 ? "start" : "end")
+            .text(d => yParams.value(d))
 
-    const bar = canvas.append("g")
-        .selectAll("g")
-        .data(series)
-        .join("g")
-        .attr("fill", ([{i}]) => colorScale(Z[i]))
-        .selectAll("rect")
-        .data(d => d)
-        .join("rect")
-        .attr("x", ([x1, x2]) => Math.min(xScale(x1), xScale(x2)))
-        .attr("y", ({i}) => yScale(Y[i]))
-        .attr("width", ([x1, x2]) => Math.abs(xScale(x1) - xScale(x2)))
-        .attr("height", yScale.bandwidth());
 
-    // if (title) bar.append("title")
-    //     .text(({i}) => title(i));
-
-    // canvas.append("g")
-    //     .attr("transform", `translate(${xScale(0)},0)`)
-    //     .call(yAxis)
-    //     .call(g => g.selectAll(".tick text")
-    //         .attr("dx", -3)
-    //         .attr("x", y => { // Find the minimum x-value for the corresponding y-value.
-    //             const x = d3.min(series, S => S.find(d => Y[d.i] === y)?.[0]);
-    //             return xScale(x) - xScale(0);
-    //         }));
+    yAxisGroup.attr("transform", "translate(" + xParams.scale(0) + ",0)")
+                .append("line")
+                    .attr("y1", 0)
+                    .attr("y2", SCREEN_DIMENSIONS.innerHeight);
 
     xAxisGroup.call(xParams.axis);
-    yAxisGroup.call(yParams.axis);
+
+    /** Couldn't find documentation for Susie Lu w/ Diverging Scale
+    // Define and build legend
+    var legend = d3.legendColor()
+        .shapeHeight(15)
+        .cells(10)
+        .orient("vertical")
+        .ascending(true)
+        .scale(colorParams.scale) 
+        .title("% Oil Production")
+        .titleWidth(30);
+
+    legendGroup.call(legend);
+    */
+
+    // Draw the rectangle and fill with gradient
+    let legend = canvas.append("g")
+        .attr("class", "legend-tick")
+        .attr("transform", "translate(" + SCREEN_DIMENSIONS.innerWidth * 1.1 + "," + SCREEN_DIMENSIONS.topMargin * 1.5 +")")
+        
+    legend.append("rect")  
+        .attr("width", 25)
+        .attr("height", 120)
+        .style("fill", "url(#linear-gradient)");
+
+    // Add Legend ticks
+    let legendTicks = ['Least', 'Average', 'Most']
+    let yIncrement = 118 / 2;
+    legendTicks.forEach((tick, i) => {
+        legend.append("line")
+                .attr("class", "legend-tick")
+                .attr("x1", 25)
+                .attr("y1", 1 + yIncrement * i)
+                .attr("x2", 32)
+                .attr("y2", 1 + yIncrement * i)
+                
+        legend.append("text")
+                .attr("class", "legend-label")
+                .attr("x", 35)
+                .attr("y", 2 + yIncrement * i)
+                .text(tick)
+    })
+
+    // Add legend title
+    legend.append("text")
+            .attr("class", "legend-title")
+            .attr("x", -35)
+            .attr("y", -20)
+            .attr("width", 20)
+            .text(colorParams.label1)
+    legend.append("text")
+            .attr("class", "legend-title")
+            .attr("x", -30)
+            .attr("y", -10)
+            .attr("width", 20)
+            .text(colorParams.label2)
+
 });
 
 
 
-// function StackedBarChart(data, {
-//     x = d => d, // given d in data, returns the (quantitative) x-value
-//     y = (d, i) => i, // given d in data, returns the (ordinal) y-value
-//     z = () => 1, // given d in data, returns the (categorical) z-value
-//     title, // given d in data, returns the title text
-//     marginTop = 30, // top margin, in pixels
-//     marginRight = 0, // right margin, in pixels
-//     marginBottom = 0, // bottom margin, in pixels
-//     marginLeft = 40, // left margin, in pixels
-//     width = 640, // outer width, in pixels
-//     height, // outer height, in pixels
-//     xType = d3.scaleLinear, // type of x-scale
-//     xDomain, // [xmin, xmax]
-//     xRange = [marginLeft, width - marginRight], // [left, right]
-//     yDomain, // array of y-values
-//     yRange, // [bottom, top]
-//     yPadding = 0.1, // amount of y-range to reserve to separate bars
-//     zDomain, // array of z-values
-//     offset = d3.stackOffsetDiverging, // stack offset method
-//     order = (series) => { // stack order method; try also d3.stackOffsetNone
-//       return [ // by default, stack negative series in reverse order
-//         ...series.map((S, i) => S.some(([, y]) => y < 0) ? i : null).reverse(),
-//         ...series.map((S, i) => S.some(([, y]) => y < 0) ? null : i)
-//       ].filter(i => i !== null);
-//     },
-//     xFormat, // a format specifier string for the x-axis
-//     xLabel, // a label for the x-axis
-//     colors = d3.schemeTableau10, // array of colors
-//   } = {})
-
-
-
-
-
-
-
-
-// // If the height is not specified, derive it from the y-domain.
-// if (height === undefined) height = yDomain.size * 25 + marginTop + marginBottom;
-// if (yRange === undefined) yRange = [height - marginBottom, marginTop];
